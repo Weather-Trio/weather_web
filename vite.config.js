c1a09eb0dbe13b488513b399f53f578b32f27532
Yi-HyeonJu <guswwn1925@gmail.com>
2024-07-06 22:17:06 +0900

    chore : 보일러 플레이트
    기타 테일윈드 설정, 기초 파일들 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindCSS from 'tailwindCSS'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindCSS()],
 })
